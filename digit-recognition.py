cb2ad1a168faca150cc79d64c0d8c75c28db13b7
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-11-29 19:53:31 +0000

    Re-wrote Linear Classifier to not use deprecated functions and to also to be consistent with DNN Classifier

--- digit-recognition.py
+++ digit-recognition.py
@@ -26,72 +26,6 @@ def LinearClassifier():
     mnist = input_data.read_data_sets("MNIST_data")
     print("\n Loaded MNIST successfully....")
 
-    # Shorten tf.contrib.learn to just 'learn'
-    learn = tf.contrib.learn
-    
-    # Define trainData as the training set of images from MNIST
-    trainData = mnist.train.images
-
-    # Define trainLabels as the training set of labels from MNIST
-    trainLabels = np.asarray(mnist.train.labels, dtype=np.int32)
-    print("\n Created training data....")
-
-    # Define testData as the testing set of images from MNIST
-    testData = mnist.test.images
-
-    # Define testData as the testing set of images from MNIST
-    testLabels = np.asarray(mnist.test.labels, dtype=np.int32)
-    print("\n Created test data....")
-
-    # Create a variable called feature_columns
-    # https://www.tensorflow.org/guide/feature_columns
-    feature_columns = learn.infer_real_valued_columns_from_input(trainData)
-
-    # Create our classifier using TensorFlow's LinearClassifier function
-    # We give this classifier 10 classes as the there are 10 outputs for our dataset (0..9)
-    linearClassifier = learn.LinearClassifier(feature_columns=feature_columns, n_classes=10)
-    print("\n Created classifier....")
-
-    # Tell TensorFlow to fit the classifier with the training set and corresponding labels in batches of 100 and steps of 1000
-    # https://www.tensorflow.org/api_docs/python/tf/keras/models/Model#fit
-    linearClassifier.fit(trainData, trainLabels, batch_size=100, steps=1000)
-    print("\n Classifier trained!")
-
-    print("\n Evaluating accuracy!")
-    # Evaluate the accuracy of our classifier after using the fit function above
-    # https://www.tensorflow.org/api_docs/python/tf/contrib/learn/evaluate
-    # Print the accuracy of our fit method as a percentage
-    percentageAccuracy = (linearClassifier.evaluate(testData, testLabels)["accuracy"])
-    print("\nTest Accuracy: {0:f}%\n".format(percentageAccuracy*100))
-    
-    print("\n Would you like to test a specific image with this classifier?")
-    print ("""
-    1.Yes
-    2.No
-    """)
-    choice=input('')
-
-    if choice=='1':
-        # Prompt the user for the image they wish to test
-        imageNum=int(input("\n Please choose an image to test as an integer (1-10000): "))
-
-        # Make a prediction using Tensorflow and our classifier we created above from our testData
-        prediction = linearClassifier.predict(np.array([testData[imageNum]], dtype=float), as_iterable=False)
-
-        # Print our prediction and display the actual image we are trying to predict
-        print("Predicted: ", prediction)
-        print("Actual: ", testLabels[imageNum])
-    else:
-        return
-
-# Function for running our DNN Classification of MNIST      
-def DNNClassifier():
-    print("\nRunning Deep Neural Network Classification....\n")
-
-    # Load the mnist dataset from TensorFlow
-    mnist = input_data.read_data_sets('MNIST_data')
-    print("\n Loaded MNIST successfully....")
-
     # Create a function to easily get our images and labels from the MNIST dataset
     def dataInput(dataset):
         return dataset.images, dataset.labels.astype(np.int32)
@@ -100,24 +34,21 @@ def DNNClassifier():
     # Reshape the with a shape of 28x28 as this represents the pixel dimensions of our images
     # https://www.tensorflow.org/guide/feature_columns
     feature_columns = [tf.feature_column.numeric_column("mnistData", shape=[28, 28])]
-    print("\n Created and reshaped feature columns....")
+    print("\nCreated and reshaped feature columns....")
 
-    # Create our classifier using TensorFlow's DNNClassifier function
+    # Create our classifier using TensorFlow's LinearClassifier function
     # We give this classifier 10 classes as the there are 10 outputs for our dataset (0..9)
     # I'll discuss this in more detail in the next notebook
-    # https://www.tensorflow.org/api_docs/python/tf/contrib/learn/DNNClassifier
-    dnnClassifier = tf.estimator.DNNClassifier(
+    # https://www.tensorflow.org/api_docs/python/tf/estimator/LinearClassifier
+    linearClassifier = tf.estimator.LinearClassifier(
                         feature_columns=feature_columns,
-                        hidden_units=[256, 32],
-                        optimizer=tf.train.AdamOptimizer(1e-4),
+                        optimizer='Ftrl',
                         n_classes=10,
-                        dropout=0.1,
-                        model_dir="./tmp/mnist_model"
                         )
-    print("\n Created classifier....")
+    print("\nCreated classifier....")
 
-     # Combine the training data and labels into one variable
-     # https://www.tensorflow.org/api_docs/python/tf/estimator/inputs/numpy_input_fn
+    # Combine the training data and labels into one variable
+    # https://www.tensorflow.org/api_docs/python/tf/estimator/inputs/numpy_input_fn
     trainingData = tf.estimator.inputs.numpy_input_fn(
             x={"mnistData": dataInput(mnist.train)[0]},
             y=dataInput(mnist.train)[1],
@@ -125,7 +56,7 @@ def DNNClassifier():
             batch_size=100,
             shuffle=True
         )
-    print("\n Created training data....")
+    print("\nCreated training data....")
 
 
     # Combine the test images and test labels into one variable
@@ -136,13 +67,137 @@ def DNNClassifier():
             num_epochs=1,
             shuffle=False
         )
-    print("\n Created test data....")
+    print("\nCreated test data....")
 
     # Tell TensorFlow to train the classifier with the training set and corresponding labels in steps of 100
     # https://www.tensorflow.org/api_docs/python/tf/estimator/Estimator#train
-    print("\n Training classifier....")
+    print("\nTraining classifier....")
+    linearClassifier.train(input_fn=trainingData, steps=1000)
+    print("\nClassifier trained!")
+
+    # Assign the images from the MNIST Test set to a variable called testingImagees
+    testingImages = mnist.test.images 
+
+    # Evaluate the accuracy of our classifier after using the fit function above
+    # https://www.tensorflow.org/api_docs/python/tf/contrib/learn/evaluate
+    # Print the accuracy of our fit method as a percentage
+    percentageAccuracy = (linearClassifier.evaluate(input_fn=testingData)["accuracy"])
+    print("\nTest Accuracy: {0:f}%\n".format(percentageAccuracy*100))
+    
+      # Print menu
+    print("\n Would you like to test a specific image with this classifier?")
+    print ("""
+    1.Yes
+    2.No
+    """)
+    
+    # Take input for the above
+    lnChoice=input('')
+
+    # Keep menu open while the user wants to test a specific image
+    while lnChoice=='1':
+        # Prompt the user for the image they wish to test
+        imageNum=int(input("\n Please choose an image to test as an integer (1-10000): "))
+
+        # Assign our prediction to a generator object called predictions, passing it in a specific image from our testing array of images
+        # Set shuffle to false to ensure we get the right image 
+        # https://www.tensorflow.org/api_docs/python/tf/estimator/Estimator#predict
+        predictions = linearClassifier.predict(input_fn=tf.estimator.inputs.numpy_input_fn(x={"mnistData": np.array([testingImages[imageNum]])}, shuffle=False))
+
+
+        # Loop through every element of our prediction object
+        for p in predictions:
+            # Extract just the probability variables from predictions
+            probList = (p['probabilities'])
+            
+            # Get the max value i.e the predicted digit from our list of probabilites
+            maxValue = max(probList)
+
+            # Print the predicted value to the screen
+            predictedValue = probList.argmax(axis=0)
+            # Round the array of probabilities to 2 decimal palces for easier reading
+            roundedArray = np.round(probList, decimals=2)
+
+            # Print our predictions
+            print("\n------------Prediction------------\n")
+            for i in range(0,10):
+                print ("Prediction of %i: %.3f" % (i,roundedArray[i]))           
+            
+            print("\nPrediction accuracy: {0:f}%\n".format(maxValue*100))
+            print("Predicted: ", predictedValue)
+
+        # Get the numbers actual value from labels in the test set of images
+        actual = dataInput(mnist.test)[1][imageNum]
+        print("Actual: ", actual)
+        print("\n------------------------------------\n")
+
+        # Prompt the user if they would like to try another image
+        print("\n Would you like to test a specific image with this classifier?")
+        print ("""
+            1.Yes
+            2.No
+            """)
+        lnChoice=input('')
+
+# Function for running our DNN Classification of MNIST      
+def DNNClassifier():
+    print("\nRunning Deep Neural Network Classification....\n")
+
+    # Load the mnist dataset from TensorFlow
+    mnist = input_data.read_data_sets('MNIST_data')
+    print("\nLoaded MNIST successfully....")
+
+    # Create a function to easily get our images and labels from the MNIST dataset
+    def dataInput(dataset):
+        return dataset.images, dataset.labels.astype(np.int32)
+
+    # Create a variable called feature_columns
+    # Reshape the with a shape of 28x28 as this represents the pixel dimensions of our images
+    # https://www.tensorflow.org/guide/feature_columns
+    feature_columns = [tf.feature_column.numeric_column("mnistData", shape=[28, 28])]
+    print("\nCreated and reshaped feature columns....")
+
+    # Create our classifier using TensorFlow's DNNClassifier function
+    # We give this classifier 10 classes as the there are 10 outputs for our dataset (0..9)
+    # I'll discuss this in more detail in the next notebook
+    # https://www.tensorflow.org/api_docs/python/tf/estimator/DNNClassifier
+    dnnClassifier = tf.estimator.DNNClassifier(
+                        feature_columns=feature_columns,
+                        hidden_units=[256, 32],
+                        optimizer=tf.train.AdamOptimizer(1e-4),
+                        n_classes=10,
+                        dropout=0.1,
+                        model_dir="./tmp/mnist_model"
+                        )
+    print("\nCreated classifier....")
+
+     # Combine the training data and labels into one variable
+     # https://www.tensorflow.org/api_docs/python/tf/estimator/inputs/numpy_input_fn
+    trainingData = tf.estimator.inputs.numpy_input_fn(
+            x={"mnistData": dataInput(mnist.train)[0]},
+            y=dataInput(mnist.train)[1],
+            num_epochs=None,
+            batch_size=100,
+            shuffle=True
+        )
+    print("\nCreated training data....")
+
+
+    # Combine the test images and test labels into one variable
+    # https://www.tensorflow.org/api_docs/python/tf/estimator/inputs/numpy_input_fn
+    testingData = tf.estimator.inputs.numpy_input_fn(
+            x={"mnistData": dataInput(mnist.test)[0]},
+            y=dataInput(mnist.test)[1],
+            num_epochs=1,
+            shuffle=False
+        )
+    print("\nCreated test data....")
+
+    # Tell TensorFlow to train the classifier with the training set and corresponding labels in steps of 100
+    # https://www.tensorflow.org/api_docs/python/tf/estimator/Estimator#train
+    print("\nTraining classifier....")
     dnnClassifier.train(input_fn=trainingData, steps=1000)
-    print("\n Classifier trained!")
+    print("\nClassifier trained!")
 
     testingImages = mnist.test.images 
 
@@ -227,4 +282,6 @@ while ans:
       exit()
     elif ans !="":
       print("\n Not Valid Choice Try again") 
+
+      
 
